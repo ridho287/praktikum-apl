c9e902807f3feb8560fd9ea262c3d2789cf02b65
ridho287 <miftahurridho0@gmail.com>
2026-03-03 21:58:06 +0800

    Upload Pertama

---
+++ kelas/pertemuan-1/pertemuan-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // string nama;
+    // cout < "Masukkan nama kamu: ";
+    // cin > nama;
+    // cout < "Halo, " < nama;
+    int apel;
+    int melon;
+    cout << "Apakah jumlah Apel == Melon? : " << (apel == melon) << endl;
+    cout << "Apakah jumlah Apel != Melon? : " << (apel != melon) << endl;
+    cout << "Apakah jumlah Apel > Melon? : " << (apel > melon) << endl;
+    cout << "Apakah jumlah Apel < Melon? : " << (apel < melon) << endl;
+    cout << "Apakah jumlah Apel >= Melon? : " << (apel >= melon) << endl;
+    cout << "Apakah jumlah Apel <= Melon? : " << (apel <= melon) << endl;
+
+    return 0;
+}
